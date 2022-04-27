64b6c00cbfbc6b9375122522a8f94a73182c7473
FPi <fpi@vdeb.kervaton.local>
2022-04-27 22:04:10 +0200

    Save checklist on change

--- __manifest__.py
+++ __manifest__.py
@@ -3,12 +3,11 @@
     'name': "CRM Lead Checklist Plugin",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Use checklists to track progress in lead generation.""",
 
     'description': """
-        Long description of module's purpose
-    """,
+        The plugin adds a checklist to lead.
+        Available checklist items can be configured by sales administrators.""",
 
     'author': "GHOSTCODERS GmbH",
     'website': "http://www.ghostcoders.com",
@@ -16,22 +15,15 @@
     'category': 'Sales/CRM',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': [
         'crm'
     ],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-
-    # only loaded in demonstration mode
-    #'demo': [
-        #'demo/demo.xml',
-    #],
 
     #'installable': True,
     #'application': False,
--- models/models.py
+++ models/models.py
@@ -4,6 +4,8 @@ import logging
 from odoo import models, fields, api
 from odoo.addons.crm.models.crm_lead import Lead
 
+_logger = logging.getLogger(__name__)
+
 class ChecklistItem(models.Model):
     _name = 'crm.lead.checklist.item'
     _description = 'Entry on a lead checklist'
@@ -20,7 +22,7 @@ class Checklist(models.Model):
 
     checklist_item_ids = fields.Many2many(
         'crm.lead.checklist.item', 'crm_lead_checklist_rel', 'lead_id', 'checklist_item_id',
-        string="Checklist Items"
+        string="Checklist Items", tracking=True
     )
 
     def _compute_progress(self):
@@ -28,3 +30,17 @@ class Checklist(models.Model):
             fin_items = len(rec.checklist_item_ids)
             max_items = len(self.env['crm.lead.checklist.item'].search([]))
             rec.progress = 100 * fin_items / max_items
+
+    @api.onchange('checklist_item_ids')
+    def _onchange_checklist(self):
+        _logger.debug('* * *  Checklist changed * * *')
+        self._compute_progress()
+
+        values = []
+        for cb in self.checklist_item_ids:
+            values.append(cb.id.origin)
+        values = list(set(values))
+
+        the_id = self.id.origin
+
+        self.write({ 'checklist_item_ids': [(6, the_id, values)] })
